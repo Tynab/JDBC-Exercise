2b750b95934b4415ba447ead3c9222b075767635
Yami An <yamiannephilim@gmail.com>
2022-06-06 20:15:39 +0700

    change db

--- bai_tap_nop/src/main/java/bai_tap_nop/connection/MySQLConnection.java
+++ bai_tap_nop/src/main/java/bai_tap_nop/connection/MySQLConnection.java
@@ -8,13 +8,13 @@ import static java.lang.Class.*;
 
 public class MySQLConnection {
 	// fields
-	private static final String url = "jdbc:mysql://34.101.161.213:3306/backendfoundation";
+	private static final String url = "jdbc:mysql://34.101.99.73:3306/backendfoundation";
 
 	// get mySQL connection
 	public static Connection getConnection() {
 		try {
 			forName("com.mysql.cj.jdbc.Driver");
-			return DriverManager.getConnection(url, "root", "");
+			return DriverManager.getConnection(url, "root", "nohit");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
